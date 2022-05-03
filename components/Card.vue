163c8791ad2c0c2763845a25bd4a66541620d007
cheeze2000 <me@cheeze2000.net>
2022-05-03 10:18:11 +0800

    feat: simple profile card

---
+++ components/Card.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+	<div class="row-span-2 text-secondary rounded-lg border-4 border-secondary">
+		<div class="h-1/3 bg-secondary" />
+		<div class="relative -top-18 mx-auto w-36 h-36">
+			<img
+				alt="hehe cat"
+				class="rounded-full"
+				src="~/assets/img/hehe-cat.png"
+			/>
+		</div>
+		<div class="relative -top-36 flex flex-col justify-evenly px-6 pt-16 h-2/3">
+			<span class="text-xl text-center">
+				GitHub @cheeze2000
+			</span>
+			<span class="text-xl text-center">
+				Discord cheeze#2000
+			</span>
+		</div>
+	</div>
+</template>
