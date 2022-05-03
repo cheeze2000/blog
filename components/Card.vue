91362ff08c959578e78f83e87529265ea7560f53
cheeze2000 <me@cheeze2000.net>
2022-05-03 16:09:34 +0800

    refactor: redesign profile card

--- components/Card.vue
+++ components/Card.vue
@@ -4,8 +4,8 @@ import { Icon } from "@iconify/vue";
 
 <template>
 	<div class="relative flex flex-col row-span-2 text-secondary rounded-lg border-4 border-secondary">
-		<div class="h-1/3 bg-secondary" />
-		<div class="absolute left-1/2 top-1/3 mx-auto w-36 h-36 -translate-x-1/2 -translate-y-1/2">
+		<div class="h-2/5 bg-secondary" />
+		<div class="absolute left-1/2 top-2/5 mx-auto w-36 h-36 -translate-x-1/2 -translate-y-1/2">
 			<img
 				alt="hehe cat"
 				class="rounded-full"
@@ -13,25 +13,23 @@ import { Icon } from "@iconify/vue";
 			/>
 		</div>
 		<div class="flex flex-col grow justify-evenly pt-18 text-xl text-secondary">
-			<div class="flex gap-3 pl-6 py-2 items-center hover:text-accent hover:bg-secondary cursor-pointer">
-				<Icon class="w-9 h-9" icon="mdi:github" />
-				<span>cheeze2000</span>
-			</div>
-			<div class="flex gap-3 pl-6 py-2 items-center hover:text-accent hover:bg-secondary cursor-pointer">
-				<Icon class="w-9 h-9" icon="mdi:discord" />
-				<span>cheeze#2000</span>
-			</div>
-			<div class="flex gap-3 pl-6 py-2 items-center hover:text-accent hover:bg-secondary cursor-pointer">
-				<Icon class="w-9 h-9" icon="mdi:email" />
-				<span>me@cheeze2000.net</span>
+			<div class="flex justify-evenly">
+				<div class="flex flex-col py-2 items-center hover:text-accent cursor-pointer">
+					<Icon class="w-9 h-9" icon="mdi:github" />
+					<span>cheeze2000</span>
+				</div>
+				<div class="flex flex-col py-2 items-center hover:text-accent cursor-pointer">
+					<Icon class="w-9 h-9" icon="mdi:discord" />
+					<span>cheeze#2000</span>
+				</div>
 			</div>
 		</div>
 		<div class="flex gap-1 justify-center py-2 border-t-4 border-t-secondary">
-			<Icon class="w-6 h-6" icon="logos:nuxt-icon" />
-			<Icon class="w-6 h-6 text-secondary" icon="ic:round-plus" />
-			<Icon class="w-6 h-6" icon="logos:tailwindcss-icon" />
-			<Icon class="w-6 h-6 text-secondary" icon="ic:round-equals" />
-			<Icon class="w-6 h-6 text-red-400" icon="ph:heart-straight-fill" />
+			<Icon class="w-7 h-7" icon="logos:nuxt-icon" />
+			<Icon class="w-7 h-7 text-secondary" icon="ic:round-plus" />
+			<Icon class="w-7 h-7" icon="logos:tailwindcss-icon" />
+			<Icon class="w-7 h-7 text-secondary" icon="ic:round-equals" />
+			<Icon class="w-7 h-7 text-red-400 hover:scale-[1.08]" icon="ph:heart-straight-fill" />
 		</div>
 	</div>
 </template>
