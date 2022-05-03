5cb4572e5ab67a3d962c2126b66b017a60b344c2
cheeze2000 <me@cheeze2000.net>
2022-05-03 16:09:11 +0800

    refactor: decrease card height

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 
 <template>
 	<div class="m-6">
-		<div class="container grid md:grid-cols-2 lg:grid-cols-3 auto-rows-[240px] gap-x-12 gap-y-6 mx-auto">
+		<div class="container grid md:grid-cols-2 lg:grid-cols-3 auto-rows-[210px] gap-x-12 gap-y-6 mx-auto">
 			<Card />
 			<MiniCard :key="i" v-for="i in 9">
 				{{ i }}
