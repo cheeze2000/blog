69517595a1ea54b01fc3f4d85a5e482e131e80ed
cheeze2000 <me@cheeze2000.net>
2022-05-05 00:03:39 +0800

    refactor: use deep css selectors

--- components/Markdown.vue
+++ components/Markdown.vue
@@ -20,48 +20,50 @@ const md = new MarkdownIt({
 	<article v-html="md.render(content)" />
 </template>
 
-<style>
-article > *:first-child {
-	@apply mt-0
-}
+<style scoped>
+article {
+	> *:first-child {
+		@apply mt-0
+	}
 
-article > *:last-child {
-	@apply mb-0
-}
+	> *:last-child {
+		@apply mb-0
+	}
 
-article h1 {
-	@apply pb-2.5 mt-9 mb-4 text-4xl text-secondary border-b border-b-secondary
-}
+	:deep(h1) {
+		@apply pb-2.5 mt-9 mb-4 text-4xl text-secondary border-b border-b-secondary
+	}
 
-article h2 {
-	@apply pb-2 mt-7 mb-4 text-3xl text-secondary border-b border-b-secondary
-}
+	:deep(h2) {
+		@apply pb-2 mt-7 mb-4 text-3xl text-secondary border-b border-b-secondary
+	}
 
-article h3 {
-	@apply mt-6 mb-4 text-2xl text-secondary
-}
+	:deep(h3) {
+		@apply mt-6 mb-4 text-2xl text-secondary
+	}
 
-article p {
-	@apply mb-4 text-lg text-secondary
-}
+	:deep(p) {
+		@apply mb-4 text-lg text-secondary
+	}
 
-article a {
-	@apply text-accent underline
-}
+	:deep(a) {
+		@apply text-accent underline
+	}
 
-article pre {
-	@apply overflow-x-auto p-4 mb-4 text-secondary bg-zinc-800 rounded
-}
+	:deep(pre) {
+		@apply overflow-x-auto p-4 mb-4 text-secondary bg-zinc-800 rounded
+	}
 
-article ol {
-	@apply pl-8 mb-4 text-lg list-decimal text-secondary
-}
+	:deep(ol) {
+		@apply pl-8 mb-4 text-lg list-decimal text-secondary
+	}
 
-article ul {
-	@apply pl-8 mb-4 text-lg list-disc text-secondary
-}
+	:deep(ul) {
+		@apply pl-8 mb-4 text-lg list-disc text-secondary
+	}
 
-article img {
-	@apply mx-auto
+	:deep(img) {
+		@apply mx-auto
+	}
 }
 </style>
