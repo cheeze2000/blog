eaaaffe107e241c2860b8614f0cb301bfa5f533d
cheeze2000 <me@cheeze2000.net>
2022-05-05 08:26:13 +0800

    refactor: replace margin with padding

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,10 @@
 </script>
 
 <template>
-	<div class="m-6">
-		<div class="container grid md:grid-cols-2 lg:grid-cols-3 auto-rows-[210px] gap-x-12 gap-y-6 mx-auto">
-			<Card />
-			<MiniCard :key="i" v-for="i in 9">
-				{{ i }}
-			</MiniCard>
-		</div>
+	<div class="container grid md:grid-cols-2 lg:grid-cols-3 auto-rows-[210px] gap-x-12 gap-y-6 p-6 mx-auto">
+		<Card />
+		<MiniCard :key="i" v-for="i in 9">
+			{{ i }}
+		</MiniCard>
 	</div>
 </template>
