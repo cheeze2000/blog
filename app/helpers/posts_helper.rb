548feef4313df137d6dbcab21ae410229d4f5ad6
cheeze2000 <me@cheeze2000.net>
2022-05-19 13:49:11 +0800

    feat: render markdown underline

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def render_markdown(content)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true, underline: true)
     markdown.render(content)
   end
 end
