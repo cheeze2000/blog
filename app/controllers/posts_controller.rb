178ea6e5588d2e771d25acbbb43fc61160ba5388
cheeze2000 <me@cheeze2000.net>
2022-05-21 14:37:55 +0800

    feat: authenticated crud routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,54 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: ENV.fetch("USERNAME"), password: ENV.fetch("PASSWORD"), except: [:index, :show]
+
   def index
-    @posts = Post.select(:id, :title, :slug)
-                 .where(published: true)
-                 .order(pinned: :desc, created_at: :desc)
-                 .page(params[:page])
+    @posts = Post.published
+                 .sorted
+                 .select(:id, :title, :slug)
+                 .page(params[:page]).without_count
   end
 
   def show
-    @post = Post.find_by(slug: params[:slug])
+    @post = Post.published.find_by(slug: params[:slug])
     redirect_to root_path unless @post
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    redirect_to edit_post_path(@post.id) if @post.save
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    redirect_to preview_post_path if @post.update(post_params)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to dashboard_path, status: :see_other
+  end
+
+  def dashboard
+    @posts = Post.sorted.select(:id, :title, :published, :pinned)
+  end
+
+  def preview
+    @post = Post.find(params[:id])
+    redirect_to root_path unless @post
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :slug, :description, :content, :published, :pinned)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root "posts#index"
 
+  get "/dashboard", to: "posts#dashboard"
   get "/posts", to: "posts#index"
+  post "/posts", to: "posts#create"
+  get "/posts/new", to: "posts#new", as: "new_post"
   get "/posts/:slug", to: "posts#show", as: "post"
+  patch "/posts/:id", to: "posts#update"
+  delete "/posts/:id", to: "posts#destroy"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  get "/posts/:id/preview", to: "posts#preview", as: "preview_post"
   get "*path", to: redirect("/")
 end
