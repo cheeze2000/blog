a81fe54dcac12ff0b20ac2343a0d66a407ec674e
cheeze2000 <me@cheeze2000.net>
2023-01-07 10:43:10 +0800

    feat: set up katex

--- resources/js/components/Markdown.vue
+++ resources/js/components/Markdown.vue
@@ -1,9 +1,12 @@
 <script setup>
 import "highlight.js/styles/atom-one-dark.css";
+import "katex/dist/katex.min.css";
 
+import { onMounted } from "vue";
 import Highlight from "highlight.js";
 import MarkdownIt from "markdown-it";
 import MarkdownItUnderline from "markdown-it-underline";
+import renderMathInElement from "katex/dist/contrib/auto-render.js";
 
 defineProps({
 	content: String,
@@ -16,6 +19,14 @@ const md = new MarkdownIt({
 });
 
 md.use(MarkdownItUnderline);
+
+onMounted(() => renderMathInElement(document.body, {
+	delimiters: [
+		{ left: "$$", right: "$$", display: true },
+		{ left: "$", right: "$", display: false },
+	],
+	throwOnError: false,
+}));
 </script>
 
 <template>
